058b3410ac1ac8d2cc5e7573b2864774fb0e814b
David Q <davquang@gmail.com>
2016-11-23 23:27:55 +0000

    improved background animation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //scripts
+var api_key = "keyiTYSW80CCENwb2";
+var art_records= "https://api.airtable.com/v0/appZBFzcRUVvRqp8i/Landmarks?api_key=" + api_key;
+
+$.get(art_records, function(data) {
+  $(data.records).each(function(i, mural){    
+    $(".murals").append(`<li>Name: ${mural.fields["Name/Description"]} <br> Where: ${mural.fields["Where"]}</li>`);
+    $(".murals").append("<img src=\"" + mural.fields.Pic[0].url + "\" width=\"400\" height=\"400\" alt=\"picture\">");
+  });
+});
