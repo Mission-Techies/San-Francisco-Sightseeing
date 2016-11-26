53149e51a3b8bcca601167c27690a87a866a366c
David Q <davquang@gmail.com>
2016-11-26 07:25:17 +0000

    list file

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var api_key = "keyiTYSW80CCENwb2";
 var art_records= "https://api.airtable.com/v0/appZBFzcRUVvRqp8i/Landmarks?api_key=" + api_key;
 
 $.get(art_records, function(data) {
-  $(data.records).each(function(i, mural){    
-    $(".murals").append(`<li>Name: ${mural.fields["Name/Description"]} <br> Where: ${mural.fields["Where"]}</li>`);
-    $(".murals").append("<img src=\"" + mural.fields.Pic[0].url + "\" width=\"400\" height=\"400\" alt=\"picture\">");
+  $(data.records).each(function(i, loc){  
+    console.log(loc);
+    $(".locations").append(`<li>Name: ${loc.fields["Name/Description"]} <br> Where: ${loc.fields["Where"]}</li>`);
+    $(".locations").append("<img src=\"" + loc.fields.Pic[0].url + "\" width=\"400\" height=\"400\" alt=\"picture\">");
   });
 });
